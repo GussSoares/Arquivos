1ea604e0351ea7d8ad677e2d3a9988d8087ed16e
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-11-05 20:09:16 -0300

    arquivos

--- threads.py
+++ threads.py
@@ -2,37 +2,56 @@
 from threading import Thread
 from Tkinter import *
 
-class Application:
-    def __init__(self, master=None):
-        self.fontePadrao = ("Arial", "10")
-        self.primeiroContainer = Frame(master)
-        self.primeiroContainer.pack()
+# class Application:
+#     def __init__(self, master=None):
+#         self.fontePadrao = ("Arial", "10")
+#         self.primeiroContainer = Frame(master, height=300, width=300)
+#         self.primeiroContainer.pack()
+#
+#         self.segundoContainer = Frame(master)
+#         self.segundoContainer.pack()
+#
+#         # self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
+#         # self.titulo["font"] = ("Arial", "10", "bold")
+#         # self.titulo.pack()
+#
+#         self.nomeLabel = Label(self.segundoContainer, font=self.fontePadrao)
+#         self.nomeLabel.pack()
+#
+#         self.tela1 = Text(self.primeiroContainer, height=5, width=10)
+#         self.tela1.pack(side=LEFT)
+#
+# class Th(Thread):
+#     def  __init__ (self, num):
+#         Thread.__init__(self)
+#         self.num = num
+#
+#     def run(self):
+# root = Tk()
+# Application(root)
+# root.mainloop()
+#
+# a = Th(1)
+# a.start()
+# b = Th(2)
+# b.start()
 
-        self.segundoContainer = Frame(master, height=300, width=300)
-        self.segundoContainer.pack()
 
-        # self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
-        # self.titulo["font"] = ("Arial", "10", "bold")
-        # self.titulo.pack()
+root = Tk()
+root.title('how to get text from textbox')
 
-        self.nomeLabel = Label(self.segundoContainer, font=self.fontePadrao)
-        self.nomeLabel.pack(side=RIGHT)
 
-        self.tela1 = Text(self.segundoContainer, height=10, width=10)
-        self.tela1["width"] = 30
-        self.tela1.pack(side=RIGHT)
+#**********************************
+mystring = StringVar()
 
-class Th(Thread):
-    def  __init__ (self, num):
-        Thread.__init__(self)
-        self.num = num
+####define the function that the signup button will do
+def getvalue():
+   Label(root, text=mystring.get()).grid(row=3, column=1)
+#*************************************
 
-    def run(self):
-        root = Tk()
-        Application(root)
-        root.mainloop()
+Label(root, text="Text to get").grid(row=0, sticky=W)  #label
+Entry(root, textvariable = mystring).grid(row=0, column=1, sticky=E) #entry textbox
 
-a = Th(1)
-a.start()
-b = Th(2)
-b.start()
+WSignUp = Button(root, text="print text", command=getvalue).grid(row=3, column=0, sticky=W) #button
+
+root.mainloop()
