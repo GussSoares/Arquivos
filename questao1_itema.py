6a84e1d832ea7a159b332c1fdf44673ce22464a9
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-11-17 16:12:08 -0300

    arquivo

--- questao1_itema.py
+++ questao1_itema.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 from multiprocessing import *
 from tkinter import *
-import threading, os
+import threading, os, time
 
-def close():
-    os.popen("pkill python")
+# def close():
+#    os.popen("pkill python")
+
 
 def refreshWrite(write, text_box):  # escrita
+
     while True:
-        write.send(text_box.get(1.0, END))  # captura o texto escrito
+       write.send(text_box.get(1.0, END))  # captura o texto escrito
 
 
 def refreshRead(read, nomeLabel):                          # leitura
+
     while True:
-        text = read.recv()                  # cria-se uma variavel auxiliar para armazear a msg escrita
-        if text != nomeLabel.cget("text"):
-            nomeLabel.config(text=text)     # seta o label para a msg escrita
+       text = read.recv()                  # cria-se uma variavel auxiliar para armazear a msg escrita
+       if text != nomeLabel.cget("text"):
+           nomeLabel.config(text=text)     # seta o label para a msg escrita
 
 
-def son(name,read,write):                         # processo pai
+def son(name,read,write):  # processo pai
+    print("Process Filho Id: " + str(os.getpid()))
     def interface(master=None):                     # criacao das janelas
         fontePadrao = ("Arial", "10")               # fonte padrao
         label = Label(master, text="Enter text")    # label de indicação
@@ -33,9 +37,10 @@ def son(name,read,write):                         # processo pai
         nomeLabel["width"] = 50
         nomeLabel.config(wraplength=350)            # quebra de linha
         nomeLabel.pack()
-        button = Button(master, text="Close", command=close)
-        button.pack()
+        # button = Button(master, text="Close", command=close)
+        # button.pack()
 
+        time.sleep(.1)
         thread1 = threading.Thread(target=refreshWrite, args=(write, text_box,)) # thread de escrita
         thread2 = threading.Thread(target=refreshRead, args=(read, nomeLabel,))  # thread de leitura
         thread1.start()                             # inicia a thread
@@ -48,7 +53,8 @@ def son(name,read,write):                         # processo pai
 
 
 def father():
-
+    print("Process Pai Id: " + str(os.getpid()))
+    time.sleep(.1)
     read1, write1 = Pipe()                          # cria os pipes
     read2, write2 = Pipe()                          # cria os pipes
 
@@ -58,12 +64,12 @@ def father():
     # p2.start()
 
     p1 = os.fork()                                  # cria os forks
-    if p1 == 0:                                     # se 0, é filho
+    if p1 == 0:                                      # se > 0, é filho
         son("SON_1", read1, write2)                 # cria processo filho1
-    else:                                           # senão é pai
-        p2 = os.fork()                              # cria pai
-        if p2 == 0:                                 # se 0, é filho
-            son("SON_2", read2, write1)             # cria processo filho2
+                                           # senão, é pai
+    p2 = os.fork()                              # cria pai
+    if p2 == 0:                                  # se > 0, é filho
+        son("SON_2", read2, write1)             # cria processo filho2
 
 if __name__ == '__main__':
     father()
