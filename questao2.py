22cf9868c9dc2f5aca400ba136f788a246982120
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-11-24 14:31:40 -0300

    arquivo

--- questao2.py
+++ questao2.py
@@ -16,7 +16,7 @@ import numpy as np
 # 3 é responsável por ler o buffer B23 e mostrar e atualizar a cada 30 s uma figura na tela
 # com seis gráficos, mostrando a evolução de cada uma destas seis variáveis
 
-b12 = Queue(6)
+b12 = Queue()
 b23 = Queue()
 
 def T1(buffer):
@@ -28,7 +28,7 @@ def T1(buffer):
         if package != "":
             pacote = package[package.index("proto"):].split(" ")[1] + " " + package[package.index("proto"):].split(" ")[4][:-2]
             print(pacote)
-            if ("TCP" in pacote) or ("UDP" in pacote):
+            if ("TCP" in pacote) or ("UDP" in pacote) or ("IGMP" in pacote):
                 buffer.put(pacote)
                 i+=1
 
@@ -40,6 +40,7 @@ def T1(buffer):
 def T2(buffer1_2, buffer2_3):
     tcp=[]
     udp=[]
+    igmp=[]
 
     def media(lista, nome):
         # print(lista, len(lista), "pacotes")
@@ -47,7 +48,7 @@ def T2(buffer1_2, buffer2_3):
             # print("media do tamanho dos pacotes:", sum(lista)/len(lista))
             return sum(lista)/len(lista)
         else:
-            print("Nenhum Pacote {}".format(nome))
+            # print("Nenhum Pacote {}".format(nome))
             return -1
 
     def variancia(lista, nome):
@@ -59,7 +60,7 @@ def T2(buffer1_2, buffer2_3):
                 soma += pow((valor-med), 2)
             variancia = soma/float(len(lista))
             return variancia
-        print("Nenhum Pacote {}".format(nome))
+        # print("Nenhum Pacote {}".format(nome))
         return -1
 
     # while True:
@@ -74,15 +75,23 @@ def T2(buffer1_2, buffer2_3):
             print("Package Received: UDP")
             udp.append(sum([int(x) for x in teste.split(" ")[1:]]))
 
+        if "IGMP" in teste:
+            print("Package Received: IGMP")
+            igmp.append(sum([int(x) for x in teste.split(" ")[1:]]))
+
     print("PACOTES TCP:",tcp)
     print("PACOTES UDP:",udp)
+    print("PACOTE IGMP:",igmp)
     print("media TCP:", media(tcp, "TCP"))
     print("variancia TCP:", variancia(tcp, "TCP"))
     print("media UDP:", media(udp, "UDP"))
     print("variancia UDP:", variancia(udp, "UDP"))
+    print("media IGMP:", media(igmp, "IGMP"))
+    print("variancia IGMP:", variancia(igmp, "IGMP"))
 
     def cria_informacoes(lista, nome):
         informacoes = []
+        informacoes.append(nome)
         informacoes.append(int(len(lista)))
         informacoes.append(float(media(lista, nome)))
         informacoes.append(float(variancia(lista, nome)))
@@ -93,6 +102,7 @@ def T2(buffer1_2, buffer2_3):
     # print(cria_informacoes(udp, "UDP"))
     buffer2_3.put(cria_informacoes(tcp, "TCP"))
     buffer2_3.put(cria_informacoes(udp, "UDP"))
+    buffer2_3.put(cria_informacoes(igmp, "IGMP"))
 
     print(buffer2_3.qsize())
 
@@ -101,14 +111,6 @@ def T2(buffer1_2, buffer2_3):
     # del tcp[:]
     # del udp[:]
 
-        # time.sleep(30)
-# p1=Process(target=T1, args=(b12,))
-# p2=Process(target=T2, args=(b12,))
-# #
-# p1.start()
-# p1.join()
-# p2.start()
-# p2.join()
 
 def T3(buffer2_3):
     x = np.linspace(0, 2, 100)
@@ -132,26 +134,25 @@ def T3(buffer2_3):
         # print(lista1)
 
 t1 = Thread(target=T1, args=(b12,))
-t2 = Thread(target=T2, args=(b12, b23,))
-t3 = Thread(target=T3, args=(b23,))
 
 t1.start()
-# t1.join()
+
 y=0
 
 while True:
     time.sleep(30-y)
     x = time.time()
-    t2.start()
-    t2.join()
-    t3.start()
-    t3.join()
+
+    t2 = Thread(target=T2, args=(b12, b23,))
+    t3 = Thread(target=T3, args=(b23,))
+
+    if (not t2.is_alive()) and (not t3.is_alive()):
+        t2.start()
+        t2.join()
+        t3.start()
+        t3.join()
+
     y = time.time() - x
-
-# t3.start()
-# t3.join()
-
-# matplot(b23)
 
 # T1(b12)
 # T2(b12, b23)
--- teste.py
+++ teste.py
@@ -1,52 +1,179 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'gustavo.ui'
+import os
+# from multiprocessing import *
+import time
+from queue import Queue
+from threading import Thread
+import time, datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+# filho T1 é responsável por ler os pacotes da placa de rede, examinar quantos bytes
+# tem o pacote e se o protocolo é TCP, UDP ou SMTP, e colocar as duas informações no buffer B12
 #
-# Created by: PyQt5 UI code generator 5.9
+# T2 é responsável por ler o buffer B12 e calcular o número, o tamanho médio e a variância dos
+# pacotes UDP, TCP e SMTP a cada 30 s e colocar estas seis informações no buffer B23
 #
-# WARNING! All changes made in this file will be lost!
+# 3 é responsável por ler o buffer B23 e mostrar e atualizar a cada 30 s uma figura na tela
+# com seis gráficos, mostrando a evolução de cada uma destas seis variáveis
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+b12 = []
+b23 = []
 
-class Ui_MainWindow(object):
-    # def soon(name, read, write):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 382)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(20, 35, 381, 281))
-        self.lineEdit.setText("")
-        self.lineEdit.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
-        self.lineEdit.setObjectName("lineEdit")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(422, 40, 341, 271))
-        self.label.setAutoFillBackground(True)
-        self.label.setText("")
-        self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
-        self.label.setObjectName("label")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 28))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+def T1(buffer):
+    i=0
+    while True:
+    # while not buffer.full():
+        package = os.popen("sudo tcpdump -i enp1s2 -c 1 -v|grep proto").read()
+        print(package)
+        if package != "":
+            pacote = package[package.index("proto"):].split(" ")[1] + " " + package[package.index("proto"):].split(" ")[4][:-2]
+            print(pacote)
+            if ("TCP" in pacote) or ("UDP" in pacote) or ("IGMP" in pacote):
+                buffer.append(pacote)
+                i+=1
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        self.timer1 = QtCore.QTimer()
-        self.timer1.timeout.connect(self.teste)
-        self.timer1.start(1)
+        print(i)
+        # i = 0
 
 
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Prompt"))
-# ======================================================================
-    def teste(self):
-        self.label.setText(self.lineEdit.text())
-# ======================================================================
+def T2(buffer1_2, buffer2_3):
+    tcp=[]
+    udp=[]
+    igmp=[]
+    i=0
+
+    def media(lista, nome):
+        # print(lista, len(lista), "pacotes")
+        if len(lista) > 0:
+            # print("media do tamanho dos pacotes:", sum(lista)/len(lista))
+            return sum(lista)/len(lista)
+        else:
+            # print("Nenhum Pacote {}".format(nome))
+            return 0
+
+    def variancia(lista, nome):
+        med = media(lista, nome)
+        soma = 0
+        variancia = 0
+        if len(lista) > 0:
+            for valor in lista:
+                soma += pow((valor-med), 2)
+            variancia = soma/float(len(lista))
+            return variancia
+        # print("Nenhum Pacote {}".format(nome))
+        return 0
+
+    while i != len(buffer1_2):
+        teste=str(buffer1_2[i])
+
+        if "TCP" in teste:
+            print("Package Received: TCP")
+            tcp.append(sum([int(x) for x in teste.split(" ")[1:]]))
+
+        if "UDP" in teste:
+            print("Package Received: UDP")
+            udp.append(sum([int(x) for x in teste.split(" ")[1:]]))
+
+        if "IGMP" in teste:
+            print("Package Received: IGMP")
+            igmp.append(sum([int(x) for x in teste.split(" ")[1:]]))
+        i+=1
+
+    print("============================================")
+    print("PACOTES TCP:",tcp)
+    print("PACOTES UDP:",udp)
+    print("PACOTE IGMP:",igmp)
+    print("media TCP:", media(tcp, "TCP"))
+    print("variancia TCP:", variancia(tcp, "TCP"))
+    print("media UDP:", media(udp, "UDP"))
+    print("variancia UDP:", variancia(udp, "UDP"))
+    print("media IGMP:", media(igmp, "IGMP"))
+    print("variancia IGMP:", variancia(igmp, "IGMP"))
+    print("============================================")
+
+    def cria_informacoes(lista, nome):
+        informacoes = []
+        informacoes.append(nome)
+        informacoes.append(int(len(lista)))
+        informacoes.append(float(media(lista, nome)))
+        informacoes.append(float(variancia(lista, nome)))
+
+        return informacoes
+
+    # print(cria_informacoes(tcp, "TCP"))
+    # print(cria_informacoes(udp, "UDP"))
+
+    buffer2_3.append(cria_informacoes(tcp, "TCP"))
+    buffer2_3.append(cria_informacoes(udp, "UDP"))
+    buffer2_3.append(cria_informacoes(igmp, "IGMP"))
+    print(len(buffer2_3))
+    print(buffer2_3)
+    # print(buffer2_3.qsize())
+
+    while not (len(buffer1_2) == 0):
+        buffer1_2.pop()
+    # del tcp[:]
+    # del udp[:]
+
+
+def T3(buffer2_3):
+    index=0
+    time=5
+    while True:
+        # x_inicial_tcp_num = buffer2_3[index][1]
+        # x_inicial_tcp_med = buffer2_3[index][2]
+        # x_inicial_tcp_var = buffer2_3[index][3]
+
+        # tcp_num = np.linspace(x_inicial_tcp_num, 2, 100)
+        plt.scatter(time, buffer2_3[index][1])
+        plt.scatter(time, buffer2_3[index+1][1])
+        plt.scatter(time, buffer2_3[index+2][1])
+
+        plt.plot(time, buffer2_3[index][1])
+        plt.plot(time, buffer2_3[index + 1][1])
+        plt.plot(time, buffer2_3[index + 2][1])
+        # plt.plot(x, x, label='num')
+        # plt.plot(x, x ** 2, label='media')
+        # plt.plot(x, x ** 3, label='variancia')
+
+        plt.xlabel('x label')
+        plt.ylabel('y label')
+
+        plt.title("Simple Plot")
+
+        plt.legend()
+
+        plt.show()
+        plt.pause(0.0001)
+        index += 3
+        time+=5
+        # time.sleep(5)
+
+
+        # lista1 = str(buffer2_3.get())
+        # print(lista1)
+
+t1 = Thread(target=T1, args=(b12,))
+
+t1.start()
+
+y=0
+
+while True:
+    time.sleep(5-y)
+    x = time.time()
+
+    t2 = Thread(target=T2, args=(b12, b23,))
+    t3 = Thread(target=T3, args=(b23,))
+
+    if (not t2.is_alive()):
+        t2.start()
+        t2.join()
+
+    t3.start()
+    t3.join()
+
+    y = time.time() - x
